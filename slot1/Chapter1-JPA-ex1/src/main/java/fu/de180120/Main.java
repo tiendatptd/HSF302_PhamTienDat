f723dcee25ffc9c1e2709b27a869616d627938d2
DatPT <tiendatptd.it@gmail.com>
2026-09-14 19:31:45 +0700

    feat(TODO 0.3): implement EmployeeDAO.save method

--- slot1/Chapter1-JPA-ex1/src/main/java/fu/de180120/Main.java
+++ slot1/Chapter1-JPA-ex1/src/main/java/fu/de180120/Main.java
@@ -1,12 +1,26 @@
 package fu.de180120;
 
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
+import fu.de180120.dao.EmployeeDAO;
+import fu.de180120.pojo.Employee;
+import fu.de180120.pojo.Gender;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hsf302FU");
-        System.out.println("EMF tao thanh cong!");
-        emf.close();
+        EmployeeDAO dao = new EmployeeDAO();
+
+        Employee emp = new Employee();
+        emp.setFullName("Nguyen Van A");
+        emp.setEmail("a.nguyen@fpt.edu.vn");
+        emp.setSalary(new BigDecimal("15000000.00"));
+        emp.setGender(Gender.MALE);
+        emp.setHireDate(LocalDate.of(2023, 5, 15));
+        emp.setActive(true);
+        System.out.println("Truoc khi save - ID: " + emp.getId());
+        dao.save(emp);
+        System.out.println("Sau khi save - ID: " + emp.getId());
+        System.out.println("Thong tin nhan vien: " + emp);
     }
 }
---
+++ slot1/Chapter1-JPA-ex1/src/main/java/fu/de180120/dao/EmployeeDAO.java
@@ -0,0 +1,30 @@
+package fu.de180120.dao;
+
+import fu.de180120.pojo.Employee;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class EmployeeDAO {
+
+    private static final EntityManagerFactory emf =
+            Persistence.createEntityManagerFactory("hsf302FU");
+
+    public void save(Employee e) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+
+            em.persist(e);
+
+            em.getTransaction().commit();
+        } catch (RuntimeException ex) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            throw ex;
+        } finally {
+            em.close();
+        }
+    }
+}

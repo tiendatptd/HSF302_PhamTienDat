faf2222033ef1b61a9218a0820b9c21c679fe8f0
DatPT <tiendatptd.it@gmail.com>
2026-09-25 14:38:54 +0700

    feat(builtin): sort and paginate students with findAll
    Refs: TODO 7

--- slot6/ch4/src/main/java/com/hsf302/ch4/service/StudentService.java
+++ slot6/ch4/src/main/java/com/hsf302/ch4/service/StudentService.java
@@ -1,10 +1,14 @@
 package com.hsf302.ch4.service;
 
 import com.hsf302.ch4.pojo.Student;
+import org.springframework.data.domain.Page;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StudentService {
     long count();
     Optional<Student> findById(Long id);
+    List<Student> findAllOrderByGpaDesc();
+    Page<Student> findPage(int pageIndex, int size, String sortField);
 }
